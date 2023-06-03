f6f88d99055736c588322aeb4e9d8e50cdb2767e
Sulfiya <sulfiasalimmundeth@gmail.com>
2023-06-03 10:49:55 +0530

    Day20 Price cart bootstrap task

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import PricingTable from './PricingTable';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PricingTable />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
